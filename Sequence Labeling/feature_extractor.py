d7d84626552b56c2d406d57f858fd1f69e739972
Gabor <csgabor14@gmail.com>
2017-04-30 17:17:12 -0400

    simplified sequence labelling

--- Sequence Labeling/feature_extractor.py
+++ Sequence Labeling/feature_extractor.py
@@ -1,91 +1,67 @@
 # -*- coding: utf-8 -*-
 """
-@author: Gabor Csapo gc1569
+@author: Gabor Csapo
 """
-import copy
 from nltk.stem import SnowballStemmer
-
-class Extractor:
-    def __init__(self):  
-        self.empty()
-        self.tags = ['=', 'POS=', 'BIO=']
-        self.stemmer = SnowballStemmer("english")
     
-    #emptying stored values when working on new file
-    def empty(self):
-        self.cache = {'prev2': {}, 'prev1': {}, 'next2': {}, 'next1': {}, 'current': {}}
+tags = ['=', 'POS=', 'BIO=']
+positions = ['prev2', 'prev1', '', 'next1', 'next2']
+stemmer = SnowballStemmer("english")
+      
+def seq_label(line, file, BIO):
+    output = ''   
+    #poping oldest value in quque
+    queue.pop(0)
+    #reading in the new value into next2
+    queue.append({})
+    if (line != None):
+        new = line.strip().split('\t')
+        for i in range(len(new)):
+            queue[4][tags[i]] = new[i]
+    #adding features to the file
+    for pos in [2, 0, 1, 3, 4]:
+        item = queue[pos]
+        equality = '='
+        #some special rules are required
+        if (pos == 2):
+            if (not item):
+                return
+            equality = ''
+            if (item['='] == '\n' or item['='] == ''):
+                file.write('\n')
+                return
+        elif (not item or item['='] == '' or item['POS='] == '.'):
+            break
         
-    def extract(self, line, file, mytags, BIO):
+        output += positions[pos] + equality + item['='] + '\t'
+        output += positions[pos] + "POS=" + item['POS='] + '\t'
+        output += positions[pos] + "STEM=" + stemmer.stem(item['=']) + '\t'
+        output += positions[pos] + "CAP=" + str(item["="][0].isupper()) + '\t'
+        if (BIO and pos != 2):
+            output += positions[pos] + "BIO=" + item['BIO='] + '\t'
+    if (BIO):
+        output += queue[2]['BIO=']
+    file.write(output + '\n')   
         
-        self.tags = mytags
-        positions = [['prev1','prev2'],['next1','next2']]
-        output = ''
-        #moving everything down in the line by one
-        self.cache['prev2'] = copy.deepcopy(self.cache['prev1'])
-        self.cache['prev1'] = copy.deepcopy(self.cache['current'])
-        self.cache['current'] = copy.deepcopy(self.cache['next1'])
-        self.cache['next1'] = copy.deepcopy(self.cache['next2'])        
-        #reading in the new value into next2
-        if (line == None):
-            self.cache['next2'] = {}
-        else:
-            new = line.strip().split('\t')
-            for i in range(len(new)):
-                self.cache['next2'][self.tags[i]] = new[i]
-        
-        #putting values for current into output
-        current = self.cache['current']
-        if (not current):
-            return
-        if (current['='] == '\n' or current['='] == ''):
-            file.write('\n')
-            return
-        output += current["="] + '\t'
-        output += "STEM=" + self.stemmer.stem(current["="]) + '\t'
-        output += "POS=" + current["POS="] + '\t'
-        output += "CAP=" + str(current["="][0].isupper()) + '\t'
-        
-        #putting values into output for the words before and after
-        for direction in positions:
-            for position in direction:
-                item = self.cache[position]
-                if (not item):
-                    break
-                if (item['='] == ''):
-                    break
-                if (item['POS='] == '.'):
-                    break
-                for tag in self.tags:
-                    output += position + tag + item[tag] + '\t'
-                output += position + "STEM=" + self.stemmer.stem(item['=']) + '\t'
-                output += position + "CAP=" + str(item["="][0].isupper()) + '\t'
-        #for the trainign we have BIO but for the test we don't
-        if (BIO):
-            output += current["BIO="]
-        file.write(output + '\n')
-    
-         
-		
-out_test = open("test.chunk", 'w')
-out_training = open("training.chunk", 'w')
 
-extractor = Extractor()
-#traingin file
-with open("WSJ_02-21.pos-chunk", "r") as in_training:
-    for line in in_training:
-        extractor.extract(line, out_training, ['=', 'POS=', 'BIO='], True)
-extractor.extract(None, out_training, ['=', 'POS=', 'BIO='], True)
-out_training.write('\n')
-#test file
-extractor.empty()
-with open("WSJ_23.pos", "r") as in_test:
-    for line in in_test:
-        extractor.extract(line, out_test, ['=', 'POS='], False)
-extractor.extract(None, out_test, ['=', 'POS='], False)
-out_test.write('\n')
+#read and tag training file
+queue = [None, None, None, None, None]
+train_output = open("training.chunk", 'w')
+with open("WSJ_02-21.pos-chunk", "r") as train_input:
+    for line in train_input:
+        seq_label(line, train_output, True)
+seq_label(None, train_output, True)
+train_output.write('\n')
+train_output.close()
+train_input.close()
 
-           
-out_test.close()
-out_training.close()
-in_test.close()
-in_training.close()
+#read and tag test file
+queue = [None, None, None, None, None]
+test_output = open("test.chunk", 'w')
+with open("WSJ_23.pos", "r") as test_input:
+    for line in test_input:
+        seq_label(line, test_output, False)
+seq_label(None, test_output, False)
+test_output.write('\n')
+test_output.close()
+test_input.close()
